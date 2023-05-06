44a4f3d0427a9f352d93f3d667ba35db256bb3cf
Rui <rui19981120.sns@gmail.com>
2023-05-06 15:28:19 +0900

    Fixed import name

--- src/renderer/components/Button/StyleList.ts
+++ src/renderer/components/Button/StyleList.ts
@@ -1,6 +1,6 @@
 import type { ButtonListType } from 'renderer/types/components/Button';
 import BaseButtonStyle from './style/base';
-import SideMenu from './style/SideMenu';
+import SideMenu from './style/sideMenu';
 import Loading from './style/loading';
 import Primary from './style/primary';
 
