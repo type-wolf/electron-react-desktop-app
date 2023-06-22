b5d68045ff3e36da4abacbdc199a14fcbaa851b1
Rui <rui19981120.sns@gmail.com>
2023-06-22 19:55:15 +0900

    Ref
    Separate tests for the Renderer process and the Main process.

---
+++ src/__tests__/renderer/App.test.tsx
@@ -0,0 +1,40 @@
+/* eslint no-unused-vars: off */
+
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { GlobalStyle } from 'rvi-system';
+import App from '../../renderer/App';
+
+window.matchMedia = jest.fn().mockImplementation((query) => {
+    return {
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    };
+});
+
+window.ResizeObserver = jest.fn().mockImplementation(() => {
+    return {
+        observe() {},
+        unobserve() {},
+        disconnect() {},
+    };
+});
+
+describe('App', () => {
+    it('should render', () => {
+        expect(
+            render(
+                <>
+                    <GlobalStyle />
+                    <App />
+                </>
+            )
+        ).toBeTruthy();
+    });
+});
