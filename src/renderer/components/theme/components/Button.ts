1c0e4391bab35eddeb4d6e1069135438d16daa25
Rui <rui19981120.sns@gmail.com>
2023-07-06 14:52:26 +0900

    Add
    Added the ability to set a theme for Components

---
+++ src/renderer/components/theme/components/Button.ts
@@ -0,0 +1,15 @@
+const Button = {
+    baseStyle: {
+        fontWeight: 'bold',
+    },
+    sizes: {},
+    variants: {
+        'with-form': {
+            bg: 'rBlue.800',
+            color: 'rWhite.800',
+        },
+    },
+    defaultProps: {},
+};
+
+export default Button;
--- src/renderer/components/theme/index.ts
+++ src/renderer/components/theme/index.ts
@@ -1,6 +1,18 @@
 import { extendTheme } from '@chakra-ui/react';
 import colors from './colors';
+import components from './components';
 
-const theme = extendTheme({ colors });
+const theme = extendTheme({
+    colors,
+    components,
+    styles: {
+        global: {
+            body: {
+                bg: 'blackAlpha.900',
+                color: 'whiteAlpha.900',
+            },
+        },
+    },
+});
 
 export default theme;
